64a4440e6ae14a3f079c9f72ef4a4629e77d8af0
gitdevjin <dev.hjshin@gmail.com>
2025-11-06 11:31:45 -0500

    CreateGroupModal Added

---
+++ src/components/Groups/GroupsPanel.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useState } from "react";
+import CreateGroupModal from "./CreateGroupModal";
+
+export default function GroupsPanel() {
+  const [displayModal, setDisplayModal] = useState(false);
+  return (
+    <div>
+      <div className="flex flex-row justify-between items-center p-2">
+        <div className="flex flex-row gap-2">
+          <div>Sort By</div>
+          <div>select box</div>
+        </div>
+        <button onClick={() => setDisplayModal(true)}>Create Group</button>
+      </div>
+
+      {displayModal && <CreateGroupModal setDisplay={setDisplayModal} />}
+    </div>
+  );
+}
